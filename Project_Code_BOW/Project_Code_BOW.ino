ed634fd131bb2f60ce6a655a4d460cbda3e8e67d
FatemaJanahi <43363498+FatemaJanahi@users.noreply.github.com>
2019-02-21 02:00:18 -0500

    Add files via upload

---
+++ Project_Code_BOW/Project_Code_BOW.ino
@@ -0,0 +1,205 @@
+/*
+Filename: Project_Code_BOW
+Authors: Fatema Janahi, Selena Nguyen, David Muir, Ciaran Farley
+Brief: Code for Bow & Screen 
+* Receives data from first Arduino Mega and displays score on LCD screen.
+*/
+
+
+// include the libraries:
+#include <LiquidCrystal.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+
+
+//LCD Screen pin setup
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+//integer for the score
+int score = 0;
+//char to receive data
+unsigned char data;
+
+//buzzer and led pins
+const int buzzer = 22;
+int led1 = 27;
+int led2 = 29;
+int led3 = 31;
+int led4 = 33;
+int led5 = 35;
+
+
+int b = 0;
+int c = 0;
+int d = 0;
+int e = 0;
+int f = 0;
+
+//function decleration 
+void screen();
+
+void setup() {
+
+  //setup buzzer and leds as outputs
+  pinMode(buzzer, OUTPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+
+  //set up screen
+  lcd.begin(16, 2);
+  lcd.clear();
+
+  //open serial port, set data rate to 9600 bps
+  Serial.begin(9600);
+
+  //begin radio transmission
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+
+void loop() {
+    while (radio.available()) {
+      // Go and read the data and put it into that variable
+      radio.read( &data, sizeof(char)); 
+      Serial.print(data);
+      delay(1000);
+      //output score to the screen and lights
+      screen();
+    }
+  }
+
+//function outputs score and turns on corresponding number of lights
+//based on data being received. Sounds buzzer once targets are hit.
+void screen()
+{
+  if (data == 48)//Score is 0
+  {
+    //all lights off
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+
+    lcd.clear();
+    lcd.print ("Score: 0");
+    lcd.setCursor(0, 1);
+    lcd.print ("Game has started!");
+  }
+
+  if (data == 49) //Score 100
+  { 
+    //One light on
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    lcd.clear();
+    lcd.print ("Score: 100");
+    lcd.setCursor(0, 1);
+    lcd.print ("Nice work!");
+
+    //sound the buzzer
+    while (b == 0) //ensure this is done only once
+    {
+      tone(buzzer, 500, 1500);
+      delay(100);
+      b = 1;
+    }
+  }
+
+  if (data == 50)
+  { digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+
+    lcd.clear();
+    lcd.print ("Score: 200");
+    lcd.setCursor(0, 1);
+    lcd.print ("Keep going!");
+
+    while (c == 0)
+    {
+      tone(buzzer, 500, 1500);
+      delay(100);
+      c = 1;
+    }
+  }
+
+  if (data == 51)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+
+    lcd.clear();
+    lcd.print ("Score: 300");
+    lcd.setCursor(0, 1);
+    lcd.print ("Wow!");
+
+    while (d == 0)
+    {
+      tone(buzzer, 500, 1500);
+      delay(100);
+      d = 1;
+    }
+  }
+
+  if (data == 52)
+  {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, LOW);
+    lcd.clear();
+    lcd.print ("Score: 400");
+    lcd.setCursor(0, 1);
+    lcd.print ("On fire!");
+
+    while (e == 0)
+    {
+      tone(buzzer, 500, 1500);
+      delay(100);
+      e = 1;
+    }
+  }
+
+  if (data == 53)
+  { digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+
+    lcd.clear();
+    lcd.print ("Score: 500");
+    lcd.setCursor(0, 1);
+    lcd.print ("YOU GET A STAMP!");
+
+    while (f == 0)
+    {
+      tone(buzzer, 500, 1500);
+      delay(100);
+      f = 1;
+    }
+  }
+}
+
+
+
+
---
+++ Project_Code_TARGET/Project_Code_TARGET.ino
@@ -0,0 +1,145 @@
+/*
+Filename: Project_Code_TARGET
+Authors: Fatema Janahi, Selena Nguyen, David Muir, Ciaran Farley
+Brief: Code for Targets 
+* Checks whether targets are knocked down and communicates with second Arduino Mega,  
+* which displays score. 
+*/
+
+
+// include the libraries:
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
+
+//pins for IR sensors
+const int IR1 = A1;
+const int IR2 = A2;
+const int IR3 = A3;
+const int IR4 = A4;
+const int IR5 = A5;
+
+//initial score
+int score = 0;
+
+//booleans of reading to send to receiver
+int IR1state = 0;
+int IR2state = 0;
+int IR3state = 0;
+int IR4state = 0;
+int IR5state = 0;
+
+//set these integers to 0 initially 
+//(to be used to stop reading sensors once targets are down).
+int a = 0;
+int b = 0;
+int c = 0;
+int d = 0;
+int e = 0;
+
+//char to store the data (score) to be sent to other Arduino Mega
+unsigned char data;
+
+//Setup:
+void setup() {
+  //set up the transmission
+  radio.begin(); 
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+  //set up IR sensors as inputs
+  pinMode(IR1, INPUT);
+  pinMode(IR2, INPUT);
+  pinMode(IR3, INPUT);
+  pinMode(IR4, INPUT);
+  pinMode(IR5, INPUT);
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+}
+
+void loop() {
+  
+  delay(2000);
+
+  data = '0'; //sets initial data to be sent as 0 (score=0);
+  
+  //if a is 0, check the first sensor
+  if (a == 0)
+  {
+    //read the state of sensor 1 (target 1)
+    IR1state = analogRead(IR1);
+  
+    //if the target is down (value found through testing)
+    if (IR1state < 400){
+      score++; //increment score
+      a++; //increment a to stop reading this sensor
+    }
+  }
+
+  //sensor two
+  if (b == 0)
+  {
+    IR2state = analogRead(IR2);
+    if (IR2state < 200) {
+      score++;
+      b++;
+    }
+  }
+
+  //sensor three
+  if (c == 0)
+  { 
+    IR3state = analogRead(IR3);
+    if (IR3state < 700) {
+      score++;
+      c++;
+    }
+  }
+
+  //sensor four
+  if (d == 0)
+  { IR4state = analogRead(IR4);
+    if (IR4state > 600) {
+      score++;
+      d++;
+    }
+  }
+
+  //sensor five
+  if (e == 0)
+  {
+    IR5state = analogRead(IR5);
+    if (IR5state < 400) {
+      score++;
+      e++;
+    }
+  }
+
+//set data based on score;
+  if (score == 0)
+  { data = '0';
+  }
+  if (score == 1)
+  { data = '1';
+  }
+  if (score == 2)
+  { data = '2';
+  }
+  if (score == 3)
+  { data = '3';
+  }
+  if (score == 4)
+  { data = '4';
+  }
+  if (score == 5)
+  { data = '5';
+  }
+
+  delay(1000);
+
+  //send data to the other Arduino
+  radio.write( &data, sizeof(unsigned char));
+}
+
